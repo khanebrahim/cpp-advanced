e7cb5ea93e237343fda19d2fea7f75fecb34265b
BengalDroid <bengaldroid@gmail.com>
2021-08-01 14:02:06 +0600

    matrix operaton

---
+++ matrix.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int A[2][3]={4,4,4,4,4,4};
+    int B[2][3]={1,1,1,1,1,1};
+    int C[2][3];
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            C[i][j]=A[i][j]+B[i][j];
+        }
+    }
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            cout<<C[i][j]<<" ";
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}
