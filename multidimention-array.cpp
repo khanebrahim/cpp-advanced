3c2f86ee5588fe6d2658386b0999c3e39071ae23
BengalDroid <bengaldroid@gmail.com>
2021-08-01 13:33:39 +0600

    row increment

--- multidimention-array.cpp
+++ multidimention-array.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 int main (){
-   int A[2][3]={{12,13,15},{17,18,20}};
-    for(int i=0;i<2;i++){
+   int A[3][3]={12,13,15,17,18,20,34,35,36};
+    for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             cout<<A[i][j]<<" ";
         }
